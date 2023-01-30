8af38862c917b0968710e64d92f00ec786f9fd37
variasco <vakin213@gmail.com>
2023-01-30 19:03:14 +0300

    added react and support of lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Counter } from "./components/Counter";
-import  "./global.scss";
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import "./global.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 export const App = () => {
   return (
-    <div>
-      asdasdasdasd
-      <Counter />
+    <div className="app">
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
