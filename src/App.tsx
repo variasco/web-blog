b5279db870bb61081c75ff1652b148d3219046d9
variasco <vakin213@gmail.com>
2023-01-31 20:15:01 +0300

    added themes and context to switch them

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import "./global.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import "./styles/index.scss";
+import { useTheme } from "./theme/useTheme";
 
-export const App = () => {
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+  
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path={"/about"} element={<AboutPageAsync />} />
@@ -18,3 +22,5 @@ export const App = () => {
     </div>
   );
 };
+
+export default App;
