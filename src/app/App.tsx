32196a54f9e2514b00b750a168d1ea4186eee7ce
variasco <vakin213@gmail.com>
2023-02-23 16:35:17 +0300

    added json-server

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable i18next/no-literal-string */
 import { Suspense } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { Navbar } from "widgets/Navbar";
