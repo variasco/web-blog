fa6ad1e021a6734c0161c1d67cc1871574543fac
Ivan Kurzenev <vakin213@gmail.com>
2023-07-22 16:56:33 +0300

    fixed tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,23 @@
 import { fireEvent, screen } from "@testing-library/react";
 import { componentRender } from "shared/lib/tests/componentRender";
 import { Sidebar } from "./Sidebar";
+import { Suspense } from "react";
 
 describe("Sidebar widget", () => {
   test("render", () => {
-    componentRender(<Sidebar />);
+    componentRender(
+      <Suspense fallback="loading...">
+        <Sidebar />
+      </Suspense>
+    );
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
   });
   test("toggle", () => {
-    componentRender(<Sidebar />);
+    componentRender(
+      <Suspense fallback="loading...">
+        <Sidebar />
+      </Suspense>
+    );
     const toggleButton = screen.getByTestId("sidebar-toggle");
     fireEvent.click(toggleButton);
     expect(screen.getByTestId("sidebar")).toHaveClass("collapsed");
